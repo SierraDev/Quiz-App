cb677c3752b863d722b1b5e46e4ed66f6c6a7a59
Sierra <35674159+SierraDev@users.noreply.github.com>
2018-08-19 07:26:10 -0400

    Delete question.js

--- js/question.js
+++
@@ -1,9 +0,0 @@
-export default function Question(question, choices, answerKey){
-    this.question = question;
-    this.choices = choices;
-    this.answerKey = answerKey;
-}
-
-Question.prototype.isCorrect = function(guessKey){
-    return guessKey === this.answerKey;
-}
